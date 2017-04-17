8a20cb67a2e05527bc47a744afaa002202081353
CXJ-HSM <851677152@qq.com>
2017-04-17 18:31:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cxj/menusub/MenuSUB.java
@@ -0,0 +1,25 @@
+package com.example.cxj.menusub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.SubMenu;
+
+
+public class MenuSUB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //TODO Auto-generated method stub
+        SubMenu sub=menu.addSubMenu(R.string.title);
+        sub.setIcon(android.R.drawable.ic_menu_search);
+        sub.add(0,1,1,R.string.local);
+        sub.add(0,2,2,R.string.network);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
